a221571eaa5bdc7399946303fa703a3c81c392cf
Tory Dannenberg <rfdberg@gmail.com>
2018-05-20 19:50:27 -0600

    Fix bubble sort and finish quick sort

--- test/bubbleSort-test.js
+++ test/bubbleSort-test.js
@@ -1,16 +1,15 @@
 import { assert } from 'chai';
+import bubbleSort from '../lib/bubbleSort';
 
 
 describe('bubble-test', function () {
-  it('should return true', function () {
-    assert.equal(true, true);
-  });
 
-  it.skip('should reverse the order of the array', function() {
-    let numArray = [3, 4, 1, 2, 5];
+  it('should reverse the order of the array', function() {
+    let numArray = [5, 3, 1, 2, 4];
+    let actual = bubbleSort(numArray);
+    let expected = [ 1, 2, 3, 4, 5 ];
 
-    bubbleSort(numArray);
-    assert.deepEqual(numArray, '[1, 2, 3, 4, 5]');
+    assert.deepEqual(actual, expected);
   })
 
 });
--- test/quickSort-test.js
+++ test/quickSort-test.js
@@ -1,10 +1,19 @@
 import { assert } from 'chai';
+import quickSort from '../lib/quickSort';
 
-describe('quick-sort-test', function () {
+
+describe('quickSort-test', function () {
   it('should return true', function () {
     assert.equal(true, true);
   });
 
+  it('should reverse the order of the array', function () {
+    let numArray = [3, 4, 1, 2, 5];
 
-  
+    let actual = quickSort(numArray);
+    let expected = [1, 2, 3, 4, 5];
+
+    assert.deepEqual(actual, expected);
+  })
+
 });
