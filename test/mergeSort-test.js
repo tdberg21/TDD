5100a2007aba0ad8a76dbcf9541c1eabff6c9f6d
Tory Dannenberg <rfdberg@gmail.com>
2018-06-06 10:55:38 -0600

    Finish merge sort

--- test/mergeSort-test.js
+++ test/mergeSort-test.js
@@ -1,8 +1,15 @@
 import { assert } from 'chai';
+import mergeSort from '../lib/mergeSort';
+
 
 describe('merge-test', function () {
-  it('should return true', function () {
-    assert.equal(true, true);
+
+  it('should reverse the order of a small array', function () {
+    let numArray = [5, 3, 1, 2, 4];
+    let actual = mergeSort(numArray);
+    let expected = [1, 2, 3, 4, 5];
+
+    assert.deepEqual(actual, expected);
   });
 
 });
